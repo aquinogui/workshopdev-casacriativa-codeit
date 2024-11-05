28b9fb8cfcb426ce9b963fe5fdced13fe111219e
Guilherme Mendes Aquino <55204265+aquinogui@users.noreply.github.com>
2024-11-05 11:11:48 -0300

    ajustando arquivos para deploy

--- server.js
+++ server.js
@@ -1,113 +1,104 @@
-console.log("iniciando servidor :)")
+// Configuração do Express
+const express = require("express");
+const app = express();
 
-//configuracao express
-const express = require("express")
-const server = express()
+// Configuração do banco de dados SQLite
+const db = require("./db");
 
-const db = require("./db")
+// Configuração dos arquivos estáticos
+app.use(express.static("public"));
 
- //DELETAR IDEIA//
- function deleteIdeia() {
-    db.run('DELETE FROM ideas WHERE id = ?', [1], function (err) {
-        if (err) return console.log(err)
-        console.log("Ideia Deletada", this)
-        return res.redirect("/ideias")
-    })
+// Configuração do req.body (para parsing de formulários)
+app.use(express.urlencoded({ extended: true }));
+
+// Configuração do Nunjucks para template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("views", {
+  express: app,
+  noCache: true,
+});
+
+// Função para deletar uma ideia
+function deleteIdeia(req, res) {
+  db.run('DELETE FROM ideas WHERE id = ?', [1], function (err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro ao deletar ideia.");
+    }
+    console.log("Ideia deletada", this);
+    return res.redirect("/ideias");
+  });
 }
 
-//arquivos estaticos//
-server.use(express.static("public"))
+// Rota principal para a página inicial
+app.get("/", function (req, res) {
+  db.all('SELECT * FROM ideas', function (err, rows) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro ao consultar banco de dados.");
+    }
 
-//reqbody//
-server.use(express.urlencoded({
-    extended: true
-}))
+    const reversedIdeas = [...rows].reverse();
+    let lastIdeas = [];
+    for (let idea of reversedIdeas) {
+      if (lastIdeas.length < 3) {
+        lastIdeas.push(idea);
+      }
+    }
 
-//template engine/nunjucks
+    return res.render("index.html", {
+      ideas: lastIdeas
+    });
+  });
+});
 
-const nunjucks = require("nunjucks")
-nunjucks.configure("views", {
-    express: server,
-    noCache: true,
-})
+// Rota para a página de todas as ideias
+app.get("/ideias", function (req, res) {
+  db.all('SELECT * FROM ideas', function (err, rows) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro ao consultar banco de dados.");
+    }
+    const reversedIdeas = [...rows].reverse();
+    return res.render("ideias.html", {
+      ideas: reversedIdeas
+    });
+  });
+});
 
-server.get("/", function (req, res) {
+// Rota para adicionar uma nova ideia
+app.post("/", function (req, res) {
+  const query = 'INSERT INTO ideas (image, title, category, description, link) VALUES (?,?,?,?,?);';
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ];
 
-    //CONSULTA DE TABELA//
-    db.all('SELECT * FROM ideas', function (err, rows) {
-        if (err) {
-            console.log(err)
-            return res.send("erro banco de dados ")
-        }
+  db.run(query, values, function (err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro ao adicionar ideia ao banco de dados.");
+    }
 
-        const reversedIdeas = [...rows].reverse()
+    return res.redirect("/ideias");
+  });
+});
 
-        let lastIdeas = []
-        for (let idea of reversedIdeas) {
-            if (lastIdeas.length < 3) {
-                lastIdeas.push(idea)
-            }
-        }
+// Rota para deletar uma ideia específica
+app.get("/ideias/:id/delete", function (req, res) {
+  const query = `DELETE FROM ideas WHERE id = ?`;
+  db.run(query, req.params.id, function (err) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro ao deletar ideia.");
+    }
 
-        return res.render("index.html", {
-            ideas: lastIdeas
-        })
-    })
+    return res.redirect("/ideias");
+  });
+});
 
-})
-
-
-
-server.get("/ideias", function (req, res) {
-    db.all('SELECT * FROM ideas', function (err, rows) {
-        if (err) return console.log(err)
-        const reversedIdeas = [...rows].reverse()
-
-        return res.render("ideias.html", {
-            ideas: reversedIdeas
-        })
-    })
-
-})
-
-
-server.post("/", function (req, res) {
-
-    //inserir dado
-    const query = 'INSERT INTO ideas (image,title,category,description,link) VALUES (?,?,?,?,?);'
-
-    const values = [
-        req.body.image,
-        req.body.title,
-        req.body.category,
-        req.body.description,
-        req.body.link,
-    ]
-
-    db.run(query, values, function (err) {
-        if (err) {
-            console.log(err)
-            return res.send("erro banco de dados ")
-        }
-
-        return res.redirect("/ideias")
-    })
-})
-
-server.get("/ideias/:id/delete", function(req, res) {
-
-    const query = `DELETE FROM ideas WHERE ID = ?`
-
-    db.run(query, req.params.id, function(err) {
-        if (err) {
-            console.log(err)
-            return res.send("Erro no Banco de Dados!")
-        }
-        
-        return res.redirect("/ideias")
-    })
-})
-
-
-//rota sevidor porta 3000
-server.listen(3000)
+// Exportando a função serverless para Vercel
+module.exports = app;
